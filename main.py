4340d925df8b9121b0469ba0889c8564c092ed50
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-31 21:13:00 +0530

    Added

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ def read_from_file():
     with open('ques_ans.txt') as file:
         for line in file:
             line=line.rstrip('\n') # removes new line character
-            country,city=line.split('/')
-            
+            country,city=line.split('/') # Country stores the word before "/" and 
+                                         # City stores the word after "/"
+
+# creating function to add new country and new city to the text file 
+
+def write_to_file(country_name,city_name):
+    with open('ques_ans.txt', 'a') as file: # here a means "append" , add new information to the file
+        file.write('\n' + country_name + '/' + city_name) 
+
+
+
 
 
 print("Ask The Expert - Capital cities of the World")
@@ -16,5 +25,13 @@ root=Tk() # creates an empty Tkinter Window
 
 root.withdraw()  # Hide Tkinter Window 
 the_world={}
+read_from_file()
+
+while True:
+    query_country=simpledialog.askstring('country','Type The Name of the Country') # the first parameter is the title , the second parameter tells the user what to do
+    if query_country in the_world:
+        result=the_world[query_country]
+        messagebox.showinfo('Answer ', 'The Capital of ' + query_country + 'is' + result +'!')
+
 
 root.mainloop()
